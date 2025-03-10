968c1c91967476e1c8292b5c3328cdd5850de386
Vishwamitra Kumar Singh <kumarvishwamitra14@gmail.com>
2025-03-10 23:34:05 +0530

    Update CSS

--- react Practics/backend/models/Signup.js
+++
@@ -1,9 +0,0 @@
-
-const  mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const data = new Schema({
-     name:String,
-     email:String,
-     password:Number,
-})
-module.exports = mongoose.model("signup" , data);
